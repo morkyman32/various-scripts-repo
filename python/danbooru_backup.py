7ae1ab762482b6562dd7404aeb80ef98a97821b9
morkyman32 <mirta.summers.x@gmail.com>
2024-12-10 17:17:28 +0100

    Update danbooru_backup.py

--- python/danbooru_backup.py
+++ python/danbooru_backup.py
@@ -1,3 +1,5 @@
+# This script backs up every danbooru (or any other configurable domain) post under the search_query which can be specified below
+
 import requests
 import sys
 import json
@@ -9,19 +11,22 @@ import base64
 # api_key_query_str -> "username:auth_key"
 # read u_auth_key && python danbooru_bk.py "${u_auth_key}"
 
-domain_address="danbooru.donmai.us"
-dir_save="danbooru_bkp/"
+search_query="(user:swpedia.nt) status:any"
+domain_address="gaybooru.app"
+dir_save="gaybooru_bkp/"
 
 api_key_query_str=''
 uname_api_key=''
+api_key=''
+username=''
 try:
     uname_api_key=sys.argv[1]
     api_key_query_str=f'--header "Authorization: Basic $(printf "%s" "{uname_api_key}" | base64)"'
     auth = (uname_api_key.split(":")[0], uname_api_key.split(":")[1])
+    api_key=uname_api_key.split(":")[1]
+    username=uname_api_key.split(":")[0]
 except IndexError:
     api_key_query_str = ''
-
-search_query="fav:yooyooko -favgroup:any"
 
 try:
     os.mkdir(f"{dir_save}")
@@ -32,7 +37,8 @@ response=["firstelem"]
 page_cnt=1
 while len(response)>0:
     if(uname_api_key!=''):
-        response = requests.get(f'https://{domain_address}/posts.json?tags={search_query}+&limit=200&page={page_cnt}',auth=auth,verify=False)
+        print(f'https://{domain_address}/posts.json?tags={search_query}+&limit=200&page={page_cnt}&api_key={api_key}&login={username}')
+        response = requests.get(f'https://{domain_address}/posts.json?tags={search_query}+&limit=200&page={page_cnt}&api_key={api_key}&login={username}',verify=False)
     else:
         response = requests.get(f'https://{domain_address}/posts.json?tags={search_query}+&limit=200&page={page_cnt}',verify=False)
     response=response.json()
@@ -44,7 +50,7 @@ while len(response)>0:
             f.write(json.dumps(item))
             f.close()
         try:
-            if ((not os.path.isfile(f"{dir_save}/{str(item['id'])}{pathlib.Path(item['file_url']).suffix}")) and (not os.path.isfile(f"{dir_save}/{str(item['id'])}.avif"))):
+            if ((not os.path.isfile(f"{dir_save}/{str(item['id'])}{pathlib.Path(item['file_url']).suffix}")) and (not os.path.isfile(f"{dir_save}/{str(item['id'])}.avif")) and (not os.path.isfile(f"{dir_save}/{str(item['id'])}.avif.ync")) and (not os.path.isfile(f"{dir_save}/{str(item['id'])}.mp4.ync")) and (not os.path.isfile(f"{dir_save}/{str(item['id'])}.json"))):
                 os.system(f"curl '{item['file_url']}' >> {dir_save}/{item['id']}{pathlib.Path(item['file_url']).suffix} --insecure")
         except KeyError:
             pass
